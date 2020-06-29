1f33b4e8956bc0864a15fd8847bea36ae1abfb69
qianjiu <qianjiu@email.com>
2020-06-29 18:05:29 +0800

    提交消息

---
+++ learn-cloud-producer/src/main/java/com/xiangzi/learncloudproducer/producer/RabbitmqSender.java
@@ -0,0 +1,44 @@
+package com.xiangzi.learncloudproducer.producer;
+
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 发送消息
+ */
+@Component
+public class RabbitmqSender {
+
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+
+    //回调函数：confirm确认消息是否正确到达Broker服务器
+    final RabbitTemplate.ConfirmCallback confirmCallback = new RabbitTemplate.ConfirmCallback() {
+        @Override
+        public void confirm(CorrelationData correlationData, boolean ack, String s) {
+            System.err.println("correlationData:" + correlationData);
+            System.err.println("s:" + s);
+            if (ack) {
+                //处理逻辑,更新数据库消息状态值为发送成功
+            } else {
+                //补偿机制或者重试
+                System.err.println("发送失败...");
+            }
+        }
+    };
+
+    //发送消息方法调用：构建自定义对象消息
+    public void sender() {
+        //实现callback接口，消息达到broker后触发回调，确认消息是否正确到达exchange中
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        //消息唯一ID
+        String messageId = "123";
+        CorrelationData correlationData = new CorrelationData(messageId);
+        rabbitTemplate.convertAndSend("order-exchange", "order.test", "hello", correlationData);
+
+    }
+}
