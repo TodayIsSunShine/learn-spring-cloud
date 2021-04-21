eb6411456f436bcf8c8f8f101a9ce0e7dac76a7c
zhangxiangxiang <874485792@qq.com>
2021-04-21 14:34:50 +0800

    修改配置文件

--- learn-cloud-consumer/src/main/java/com/xiangzi/learncloudconsumer/consumer/RabbitmqReceiver.java
+++ learn-cloud-consumer/src/main/java/com/xiangzi/learncloudconsumer/consumer/RabbitmqReceiver.java
@@ -1,3 +1,4 @@
+/*
 package com.xiangzi.learncloudconsumer.consumer;
 
 import com.rabbitmq.client.Channel;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
+//@Component
 public class RabbitmqReceiver {
     //配置监听的哪一个队列，同时在没有queue和exchange的情况下会去创建并建立绑定关系
     @RabbitListener(bindings = @QueueBinding(
@@ -22,18 +23,22 @@ public class RabbitmqReceiver {
         //消费者操作
         System.out.println("---------收到消息，开始消费---------");
 
-        /**
+        */
+/**
          * Delivery Tag 用来标识信道中投递的消息。RabbitMQ 推送消息给 Consumer 时，会附带一个 Delivery Tag，
          * 以便 Consumer 可以在消息确认时告诉 RabbitMQ 到底是哪条消息被确认了。
          * RabbitMQ 保证在每个信道中，每条消息的 Delivery Tag 从 1 开始递增。
-         */
+         *//*
+
         String messageStr = new String(message.getBody());
         System.out.println("消费者接受到的消息：" + messageStr);
         try {
-            /**
+            */
+/**
              *  multiple取值为 false时，表示通知RabbitMQ当前消息被确认
              *  如果为 true则额外将比第一个参数指定的deliveryTag小的消息一并确认
-             */
+             *//*
+
             boolean multiple = false;
             if (messageStr.equals("hello")) {
                 long deliveryTag = message.getMessageProperties().getDeliveryTag();
@@ -44,15 +49,18 @@ public class RabbitmqReceiver {
 
         } catch (Exception e) {
             e.printStackTrace();
-            /**
+            */
+/**
              * 如果channel.basicNack(8, true, true);表示deliveryTag=8之前未确认的消息都处理失败且将这些消息重新放回队列中。
              * 如果channel.basicNack(8, true, false);表示deliveryTag=8之前未确认的消息都处理失败且将这些消息直接丢弃。
              * 如果channel.basicNack(8, false, true);表示deliveryTag=8的消息处理失败且将该消息重新放回队列。
              * 如果channel.basicNack(8, false, false);表示deliveryTag=8的消息处理失败且将该消息直接丢弃。
-             */
+             *//*
+
             //丢弃掉这个消息
             channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
 
         }
     }
 }
+*/
