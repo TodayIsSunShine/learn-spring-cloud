b31de044009fb05d098f074718ecddfd1097671b
zhangxiangxiang <874485792@qq.com>
2021-09-07 16:01:01 +0800

    避免重复消费

--- learn-cloud-consumer/src/main/java/com/xiangzi/learncloudconsumer/consumer/DeadLetterQueueConsumer.java
+++ learn-cloud-consumer/src/main/java/com/xiangzi/learncloudconsumer/consumer/DeadLetterQueueConsumer.java
@@ -7,16 +7,28 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashSet;
 
 
+/**
+ * 监听延迟消息
+ */
 @Component
 public class DeadLetterQueueConsumer {
+
+    //避免重复消费消息
+    private static HashSet hashSet = new HashSet();
 
     @RabbitListener(queues = "my_demo_queue") //用来监听队列里面的消息
     @RabbitHandler
     public void receive(Message message, Channel channel) throws IOException {
         String msg = new String(message.getBody());
-        System.err.println(msg);
-        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        if (!hashSet.contains(msg)) {
+            System.err.println("接受到的msg：" + msg);
+            hashSet.add(msg);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } else {
+            System.err.println("重复消费了..");
+        }
     }
 }
